f58d38de56de442e2195985d4426d45487e17030
SHILPA SWETH <shilpasweth@gmail.com>
2017-05-07 22:11:45 +0400

    Initial Submission

--- app/src/main/java/com/shilpasweth/android_bus_tracker/BusInfo.java
+++ app/src/main/java/com/shilpasweth/android_bus_tracker/BusInfo.java
@@ -1,5 +1,7 @@
 package com.shilpasweth.android_bus_tracker;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Lenovo on 4/8/2017.
  */
@@ -11,9 +13,9 @@ public class BusInfo {
 
     private String vehicle_license;
 
-    private float latitude;
+    private double latitude;
 
-    private float longitude;
+    private double longitude;
 
     public String last_updated;
 
@@ -23,10 +25,28 @@ public class BusInfo {
 
     public String getVehicleLicense(){return vehicle_license;}
 
-    public float getLatitude(){return latitude;}
+    public double getLatitude(){return latitude;}
 
-    public float getLongitude(){return longitude;}
+    public double getLongitude(){return longitude;}
 
     public String getLastUpdated(){return last_updated;}
+
+    public void putVehicleId(String v_id){vehicle_id=v_id;}
+
+    public void putVehicleType(String v_type){vehicle_type=v_type;}
+
+    public void putVehicleLicense(String v_license){vehicle_license=v_license;}
+
+    public void putLatitude(float lat){
+        BigDecimal roundfinalPrice = new BigDecimal(lat).setScale(3, BigDecimal.ROUND_HALF_UP);
+        latitude=roundfinalPrice.doubleValue();
+    }
+
+    public void putLongitude(float lon){
+        BigDecimal roundfinalPrice = new BigDecimal(lon).setScale(3, BigDecimal.ROUND_HALF_UP);
+        longitude=roundfinalPrice.doubleValue();
+    }
+
+    public void putLastUpdated(String last_up){last_updated=last_up;}
 
 }
--- app/src/main/java/com/shilpasweth/android_bus_tracker/MapsActivity.java
+++ app/src/main/java/com/shilpasweth/android_bus_tracker/MapsActivity.java
@@ -1,34 +1,70 @@
 package com.shilpasweth.android_bus_tracker;
 
-import android.content.Context;
-import android.support.v4.app.Fragment;
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, MapsPresenterImpl.View {
 
     private GoogleMap mMap;
 
-    private static List<BusInfo> mBuses;
+    private static List<BusInfo> mBuses= new ArrayList<>();;
     private MapsPresenter mapsPresenter;
+
+    SupportMapFragment mapFragment;
+
+    String url = "https://bustracker-nitt.000webhostapp.com/poll.php";
+
+    int MY_PERMISSIONS_REQUEST_INTERNET=0;
+
+    static int busno=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        fetchBuses();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(MapsActivity.this);
+
+        final CountDownTimer mapRefresh = new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                //mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+
+                fetchBuses();
+                this.start();
+            }
+        };
+        mapRefresh.start();
+
     }
 
     void setPresenterView(List<BusInfo> buses) {
@@ -43,6 +79,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    public void busFocus(View view){
+        if(mBuses.size()!=0) {
+            busno = (busno + 1) %mBuses.size();
+            LatLng sydney = new LatLng(mBuses.get(busno).getLatitude(), mBuses.get(busno).getLongitude());
+
+            // mMap.setMinZoomPreference(18.0f);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,18.0f));
+        }
+    }
 
 
 
@@ -54,14 +99,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
+     *
+     *
      */
+    public void fetchBuses(){
+        StringRequest jsObjRequest = new StringRequest
+                (Request.Method.GET, url, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        // mTxtDisplay.setText("Response: " + response.toString());
+                        // Toast.makeText(MapsActivity.this,response,Toast.LENGTH_LONG).show();
+
+                        // Toast.makeText(MapsActivity.this,dataJsonObj.toString(),Toast.LENGTH_LONG).show();
+                        JSONArray dataJsonArr=null;
+                        try {
+                            dataJsonArr = new JSONArray(response);
+                            //Toast.makeText(MapsActivity.this,"dataJsonArr success",Toast.LENGTH_LONG).show();
+
+                            int no=dataJsonArr.length();
+
+                            //Toast.makeText(MapsActivity.this,"Length "+no,Toast.LENGTH_LONG).show();
+                            for(int i=0;i<no;i++){
+                                JSONObject c = dataJsonArr.getJSONObject(i);
+                                BusInfo bus= new BusInfo();
+                                bus.putVehicleId(c.getString("vehicleId"));
+                                bus.putVehicleType(c.getString("vehicleType"));
+                                bus.putVehicleLicense(c.getString("licenseNumber"));
+                                bus.putLatitude(Float.valueOf(c.getString("lat")));
+                                bus.putLongitude(Float.valueOf(c.getString("lon")));
+                                bus.putLastUpdated(c.getString("lastUpdatedAt"));
+
+                                mBuses.add(bus);
+
+
+                            }
+
+
+                            onMapRefresh();
+                            // Toast.makeText(MapsActivity.this,"Length "+mBuses.size(),Toast.LENGTH_LONG).show();
+                            //  Toast.makeText(MapsActivity.this,c.toString(),Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            //Toast.makeText(MapsActivity.this,"dataJsonArr fail",Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+
+                        //Toast.makeText(MapsActivity.this,dataJsonArr.length(),Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        //Toast.makeText(MapsActivity.this,"Error",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsObjRequest);
+    }
+
+    public void onMapRefresh(){
+
+        // Add a marker in Sydney and move the camera
+        //while(mBuses.size()==0){}
+        mMap.clear();
+        LatLng sydney = new LatLng(10.761, 78.816);
+        //Toast.makeText(MapsActivity.this,"Out Loop",Toast.LENGTH_LONG).show();
+        for(int i=0;i<mBuses.size();i++)
+        {
+            //Toast.makeText(MapsActivity.this,"In Loop",Toast.LENGTH_LONG).show();
+            LatLng bus = new LatLng(mBuses.get(i).getLatitude(), mBuses.get(i).getLongitude());
+            if(i==1)
+                sydney=bus;
+            if(mBuses.get(i).getVehicleType().equalsIgnoreCase("generalBus")){
+                mMap.addMarker(new MarkerOptions().position(bus).title("General Bus").icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_marker_general)));
+            }
+            if(mBuses.get(i).getVehicleType().equalsIgnoreCase("boysBus")){
+                mMap.addMarker(new MarkerOptions().position(bus).title("Boy's Bus").icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_marker_boys)));
+            }
+            if(mBuses.get(i).getVehicleType().equalsIgnoreCase("girlsBus")){
+                mMap.addMarker(new MarkerOptions().position(bus).title("Girl's Bus").icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_marker_girls)));
+            }
+
+        }
+       // mMap.setMinZoomPreference(18.0f);
+       //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //while(mBuses.size()==0){}
+        LatLng sydney = new LatLng(10.762, 78.816);
+        //Toast.makeText(MapsActivity.this,"Out Loop",Toast.LENGTH_LONG).show();
+
+        mMap.setMinZoomPreference(15.0f);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,18.0f));
     }
 }
